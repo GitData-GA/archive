61873dbe1f0425364e33429bac0785b9b3edbd27
admin-gd <127959916+admin-gd@users.noreply.github.com>
2023-12-22 23:13:12 -0500

    Update extract_info.py

--- .github/scripts/extract_info.py
+++ .github/scripts/extract_info.py
@@ -11,9 +11,11 @@ def extract_info_from_html(html_content):
     publication_date = publication_date['content'] if publication_date else 'Publication date not found'
     abstract = soup.find('meta', {'name': 'description'})
     abstract = abstract['content'] if abstract else 'Abstract not found'
+    subject = soup.find('div', {'class': 'side-block_section'}).find('span', {'class': 'head'}, string='Subject')
+    subject = subject.find_next('li').get_text() if subject else 'Subject not found'
     url = soup.find('meta', {'property': 'og:url'})
     url = url['content'] if url else 'URL not found'
-    return {'title': title, 'authors': authors, 'publication_date': publication_date, 'abstract': abstract, 'url': url}
+    return {'title': title, 'authors': authors, 'publication_date': publication_date, 'abstract': abstract, 'subject': subject, 'url': url}
 
 def main():
     output_data = []
@@ -24,7 +26,7 @@ def main():
                 with open(file_path, 'r', encoding='utf-8') as f:
                     html_content = f.read()
                     info = extract_info_from_html(html_content)
-                    if all(value != 'Title not found' and value != 'Publication date not found' and value != 'Abstract not found' and value != 'URL not found' for value in info.values()):
+                    if all(value != 'Title not found' and value != 'Publication date not found' and value != 'Abstract not found' and value != 'Subject not found' and value != 'URL not found' for value in info.values()):
                         output_data.append(info)
     with open('info.json', 'w') as json_file:
         json.dump(output_data, json_file, indent=2)
