c66bbe5599900e97052d640a0f6d15d69f0b0924
admin-gd <127959916+admin-gd@users.noreply.github.com>
2023-12-22 19:34:26 -0500

    Update extract_info.py

--- .github/scripts/extract_info.py
+++ .github/scripts/extract_info.py
@@ -4,18 +4,19 @@ from bs4 import BeautifulSoup
 
 def extract_info_from_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
-
-    title = soup.find('meta', {'name': 'citation_title'})['content'] if soup.find('meta', {'name': 'citation_title'}) else 'Title not found'
+    title = soup.find('meta', {'name': 'citation_title'})
+    title = title['content'] if title else 'Title not found'
     authors = [meta['content'] for meta in soup.find_all('meta', {'name': 'citation_author'})]
-    publication_date = soup.find('meta', {'name': 'citation_publication_date'})['content'] if soup.find('meta', {'name': 'citation_publication_date'}) else 'Publication date not found'
-    abstract = soup.find('meta', {'name': 'description'})['content'] if soup.find('meta', {'name': 'description'}) else 'Abstract not found'
-    url = soup.find('meta', {'property': 'og:url'})['content'] if soup.find('meta', {'property': 'og:url'}) else 'URL not found'
+    publication_date = soup.find('meta', {'name': 'citation_publication_date'})
+    publication_date = publication_date['content'] if publication_date else 'Publication date not found'
+    abstract = soup.find('meta', {'name': 'description'})
+    abstract = abstract['content'] if abstract else 'Abstract not found'
+    url = soup.find('meta', {'property': 'og:url'})
+    url = url['content'] if url else 'URL not found'
     return {'title': title, 'authors': authors, 'publication_date': publication_date, 'abstract': abstract, 'url': url}
 
 def main():
     output_data = []
-
-    # Scan through HTML files in the repository
     for root, dirs, files in os.walk('.'):
         for file in files:
             if file.endswith('.html'):
@@ -23,8 +24,8 @@ def main():
                 with open(file_path, 'r', encoding='utf-8') as f:
                     html_content = f.read()
                     info = extract_info_from_html(html_content)
-                    output_data.append(info)
-
+                    if all(value != 'Title not found' and value != 'Publication date not found' and value != 'Abstract not found' and value != 'URL not found' for value in info.values()):
+                        output_data.append(info)
     with open('info.json', 'w') as json_file:
         json.dump(output_data, json_file, indent=2)
 
