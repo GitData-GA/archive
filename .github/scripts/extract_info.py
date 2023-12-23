f563e5344921acb3db6609df5ffb649083a09ef3
admin-gd <127959916+admin-gd@users.noreply.github.com>
2023-12-22 19:23:16 -0500

    Update extract_info.py

--- .github/scripts/extract_info.py
+++ .github/scripts/extract_info.py
@@ -10,13 +10,6 @@ def extract_info_from_html(html_content):
     publication_date = soup.find('meta', {'name': 'citation_publication_date'})['content'] if soup.find('meta', {'name': 'citation_publication_date'}) else 'Publication date not found'
     abstract = soup.find('meta', {'name': 'description'})['content'] if soup.find('meta', {'name': 'description'}) else 'Abstract not found'
     url = soup.find('meta', {'property': 'og:url'})['content'] if soup.find('meta', {'property': 'og:url'}) else 'URL not found'
-
-    print(f'Title: {title}')
-    print(f'Authors: {authors}')
-    print(f'Publication Date: {publication_date}')
-    print(f'Abstract: {abstract}')
-    print(f'URL: {url}')
-
     return {'title': title, 'authors': authors, 'publication_date': publication_date, 'abstract': abstract, 'url': url}
 
 def main():
@@ -32,9 +25,8 @@ def main():
                     info = extract_info_from_html(html_content)
                     output_data.append(info)
 
-    # Write the extracted information to a JSON file
-    with open('info.json', 'w') as json_file:
-        json.dump(output_data, json_file, indent=2)
+    # Print the extracted information as JSON
+    print(json.dumps(output_data, indent=2))
 
 if __name__ == '__main__':
     main()
