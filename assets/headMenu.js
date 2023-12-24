c3d57a033dbe7ac2610d9830ceca9dc39c348b92
Lee <120439693+lygitdata@users.noreply.github.com>
2023-12-24 00:46:29 -0500

    Create headMenu.js

---
+++ assets/headMenu.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var mobile = document.getElementById('mobileMenu');
+    if (mobile) {
+        var content = `
+                    <ul>
+                        <li><a href="https://archive.gd.edu.kg/about/">About</a></li>
+                        <li><a href="https://archive.gd.edu.kg/terms/">Terms</a></li>
+                        <li><a href="https://archive.gd.edu.kg/submit/">Submit</a></li>
+                    </ul>
+        `;
+        mobile.innerHTML = content;
+    } else {
+        console.error('Container not found. Make sure the div with id "mobileMenu" exists.');
+    }
+    var normal = document.getElementById('normalMenu');
+    if (normal) {
+        var content = `
+                    <div id="normalMenu" class="menu-holder">
+                        <a class="nav-menu" href="https://archive.gd.edu.kg/about/">About</a>
+                        <a class="nav-menu" href="https://archive.gd.edu.kg/terms/">Terms</a>
+                        <a class="nav-menu" href="https://archive.gd.edu.kg/submit/">Submit</a>
+                    </div>
+        `;
+        normal.innerHTML = content;
+    } else {
+        console.error('Container not found. Make sure the div with id "normalMenu" exists.');
+    }
+});
