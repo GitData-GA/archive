2d67d6489faad6563ef53d4545a2213076b135ba
Lee <120439693+lygitdata@users.noreply.github.com>
2023-12-24 00:38:05 -0500

    Update footerMenu.js

--- assets/footerMenu.js
+++ assets/footerMenu.js
@@ -4,17 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
         var content = `
           <div class="row">
               <div class="col" style="text-align: left; font-size:12px;">
-                  <p>Useful API</p>
+                  <p>API</p>
                   <a href="https://archive.gd.edu.kg/sitemap.xml">Sitemap</a><br>
                   <a href="https://archive.gd.edu.kg/info.json">Feed</a><br>
-              </div>
-              <div class="col" style="text-align: left; font-size:12px;">
-                  <p>Test 1</p>
-                  <p>Test 1-1<br>Test 1-2</p>
-              </div>
-              <div class="col" style="text-align: left; font-size:12px;">
-                  <p>Test 2</p>
-                  <p>Test 2-1<br>Test 2-2</p>
               </div>
           </div>
         `;
