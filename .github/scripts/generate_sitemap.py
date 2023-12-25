7eeb43794bb16f276dc92382d86ad00d62bb3f98
Lee <120439693+lygitdata@users.noreply.github.com>
2023-12-25 17:08:04 -0500

    Update generate_sitemap.py

--- .github/scripts/generate_sitemap.py
+++ .github/scripts/generate_sitemap.py
@@ -1,36 +1,64 @@
 import os
-from datetime import datetime
+import json
+import re
+from bs4 import BeautifulSoup
+from operator import itemgetter
 
-def calculate_priority(level):
-    base_priority = 1.0
-    decay_factor = 0.9
-    return base_priority * (decay_factor ** level)
+def extract_info_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    title = soup.find('meta', {'name': 'citation_title'})
+    title = title['content'] if title else 'Title not found'
+    
+    authors_meta = soup.find_all('meta', {'name': 'citation_author'})
+    authors = []
+    for author_meta in authors_meta:
+        author_name_parts = author_meta['content'].split(', ')
+        formatted_author_name = ' '.join(reversed(author_name_parts))
+        authors.append(formatted_author_name)
+    
+    version = soup.find('meta', {'name': 'version'})
+    version = version['content'] if version else 'Version not found'
+    publication_date = soup.find('meta', {'name': 'citation_publication_date'})
+    publication_date = publication_date['content'] if publication_date else 'Publication date not found'
+    abstract = soup.find('meta', {'name': 'description'})
+    abstract = abstract['content'] if abstract else 'Abstract not found'
+    side_block_sections = soup.find_all('div', {'class': 'side-block_section'})    
+    subject = 'Subject not found'    
+    for section in side_block_sections:
+        span_head = section.find('span', {'class': 'head'})
+        if span_head and span_head.get_text() == 'Subject':
+            subject = section.find('li').get_text()
+            break
+    keywords = []    
+    for section in side_block_sections:
+        span_head = section.find('span', {'class': 'head'})
+        if span_head and span_head.get_text() == 'Keywords':
+            keywords_element = section.find('ul')
+            if keywords_element:
+                keywords = [keyword.strip() for keyword in keywords_element.get_text(separator='\n').split('\n') if keyword.strip()]
+                break
+    url = soup.find('meta', {'property': 'og:url'})
+    url = url['content'] if url else 'URL not found'
+    
+    return {'title': title, 'version': version, 'authors': authors, 'publication_date': publication_date, 'keywords': keywords, 'abstract': abstract, 'subject': subject, 'url': url}
 
-def generate_sitemap():
-    sitemap_header = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-    sitemap_footer = '</urlset>'
-
-    urls = []
-
+def main():
+    output_data = []
     for root, dirs, files in os.walk('.'):
         for file in files:
-            if (file.endswith('.html') or file.endswith('.pdf')) and not file.endswith('404.html'):
+            if file.endswith('.html'):
                 file_path = os.path.join(root, file)
-                if file == "index.html":
-                    file_path = os.path.dirname(file_path)
-                file_url = f'https://archive.gd.edu.kg/{file_path[2:]}/'
-                if file.endswith('.pdf'):
-                    file_url = file_url.rstrip('/')
-                if file_url == "https://archive.gd.edu.kg//":
-                    file_url = "https://archive.gd.edu.kg/"
-                file_priority = calculate_priority(file_path.count('/'))
-                last_modified = datetime.utcnow().replace(microsecond=0).isoformat() + "+00:00"
-                urls.append(f'  <url>\n    <loc>{file_url}</loc>\n    <lastmod>{last_modified}</lastmod>\n    <priority>{file_priority:.2f}</priority>\n  </url>')
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    html_content = f.read()
+                    info = extract_info_from_html(html_content)
+                    if all(value != 'Version not found' and value != 'Title not found' and value != 'Publication date not found' and value != 'Abstract not found' and value != 'Subject not found' and value != 'URL not found' for value in info.values()):
+                        output_data.append(info)
 
-    sitemap_content = '\n'.join(urls)
+    # Sort by publication_date, then 14 digits after the URL, and finally by version
+    output_data = sorted(output_data, key=itemgetter('publication_date', 'url', 'version'))
 
-    with open('sitemap.xml', 'w') as sitemap_file:
-        sitemap_file.write(f'{sitemap_header}{sitemap_content}\n{sitemap_footer}')
+    with open('info.json', 'w') as json_file:
+        json.dump(output_data, json_file, indent=2)
 
 if __name__ == '__main__':
-    generate_sitemap()
+    main()
