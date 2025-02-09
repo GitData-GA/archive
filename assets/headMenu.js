2df28278695635d9050916ff7b6fc95405f2999e
Lee <120439693+lygitdata@users.noreply.github.com>
2025-02-09 23:01:36 +0100

    Update headMenu.js

--- assets/headMenu.js
+++ assets/headMenu.js
@@ -6,16 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <a class="nav-menu" href="https://archive.gd.edu.kg/about/">About</a>
                         <a class="nav-menu" href="https://archive.gd.edu.kg/terms/">Terms</a>
                         <a class="nav-menu" href="https://archive.gd.edu.kg/submit/">Submit</a>
-                        <div id="loginDesktop" style="display: inline-block;">
-                            <a class="nav-menu" href="#" id="loginDesktopLink" style="display: flex; align-items: center;">
-                                <img src="https://archive.gd.edu.kg/assets/lock.svg" alt="lock_img" height="16px" />&nbsp;Login
-                            </a>
-                        </div>
-                        <div id="logoutDesktop" style="display: none;">
-                            <a class="nav-menu" href="#" id="logoutDesktopLink" style="display: flex; align-items: center;">
-                                <img src="https://archive.gd.edu.kg/assets/logout.svg" alt="logout_img" height="16px" />&nbsp;Logout
-                            </a>
-                        </div>
+                        <a class="nav-menu" href="https://portal.archive.gd.edu.kg/"><img src="https://archive.gd.edu.kg/assets/lock.svg" alt="lock_img" width="16px" height="16px" />&nbsp;Portal & Submit</a>
                     </div>
         `;
         normal.innerHTML = content;
@@ -32,16 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <li><a href="https://archive.gd.edu.kg/about/">About</a></li>
                     <li><a href="https://archive.gd.edu.kg/terms/">Terms</a></li>
                     <li><a href="https://archive.gd.edu.kg/submit/">Submit</a></li>
-                    <li id="loginMobile">
-                        <a href="#" id="loginMobileLink">
-                            <img src="https://archive.gd.edu.kg/assets/lock.svg" alt="lock_img" height="16px" />&nbsp;Login
-                        </a>
-                    </li>
-                    <li id="logoutMobile" style="display: none;">
-                        <a href="#" id="logoutMobileLink">
-                            <img src="https://archive.gd.edu.kg/assets/logout.svg" alt="logout_img" height="16px" />&nbsp;Logout
-                        </a>
-                    </li>
+                    <li><a class="nav-menu" href="https://portal.archive.gd.edu.kg/"><img src="https://archive.gd.edu.kg/assets/lock.svg" alt="lock_img" width="16px" height="16px" />&nbsp;Portal & Submit</a></li>
                 </ul>
         `;
         mobile.innerHTML = content;
